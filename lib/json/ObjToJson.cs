62a331caedf723792e34f357fa6a58f968dd8372
la0gke <125958751+la00gke@users.noreply.github.com>
2023-03-31 10:54:12 +0800

    Add files via upload

---
+++ lib/json/ObjToJson.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using Newtonsoft.Json;
+
+namespace DomainInfo.lib.json
+{
+    internal class ObjToJson
+    {
+        public static string objTojson(SearchResultCollection searchResults)
+        {
+            Dictionary<string, List<object>> results = new Dictionary<string, List<object>>();
+
+            foreach (SearchResult result in searchResults)
+            {
+                List<object> resultvalues = new List<object>();
+                string resultname = result.Path;
+                // Console.WriteLine(resultname);
+                DirectoryEntry directoryEntry = result.GetDirectoryEntry();
+
+                Dictionary<string, List<object>> properties = new Dictionary<string, List<object>>();
+
+                foreach (string propertyName in directoryEntry.Properties.PropertyNames)
+                {
+                    PropertyValueCollection propertyValues = directoryEntry.Properties[propertyName];
+                    List<object> values = new List<object>();
+
+                    for (int i = 0; i < propertyValues.Count; i++)
+                    {
+                        values.Add(propertyValues[i]);
+                    }
+                    properties.Add(propertyName, values);
+                }
+                resultvalues.Add(properties);
+                results.Add(resultname, resultvalues);
+            }
+            string Objjson = JsonConvert.SerializeObject(results, Formatting.Indented);
+            return Objjson;
+        }
+    }
+}

608aac0a3bbb346e51966a31d3bd01024a589f35
la0gke <125958751+la00gke@users.noreply.github.com>
2023-03-31 10:05:49 +0800

    Version 0.01 Initial commit
    Module One，domain information collection

---
+++ LDAPCoon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.DirectoryServices;
+
+namespace DomainInfo
+{
+    internal class LDAPCoon
+    {
+        public static int Port;
+        public static string DomainName;
+
+        public static DirectorySearcher Searcher()
+        {
+            DomainName = Environment.GetEnvironmentVariable("USERDNSDOMAIN");
+            Port = 389;
+            DirectoryEntry entry = new DirectoryEntry($"LDAP://{DomainName}:{Port}", null, null, AuthenticationTypes.Secure);
+            DirectorySearcher Searcher = new DirectorySearcher(entry);
+            return Searcher;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using DomainInfo.Module.DomainInfo;
+using System;
+using System.Collections.Generic;
+
+namespace DomainInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> UserLists = ObjectInfo.GetUsers();
+            Console.WriteLine(UserLists[0]);
+            ObjectInfo.GetDomainCs();
+            ObjectInfo.GetComputers();
+            ObjectInfo.GetGroups();
+            ObjectInfo.GetSPNs();
+            ObjectInfo.GetOUs();
+            ObjectInfo.GetGPOs();
+            ObjectInfo.GetAllowDelegations();
+            ObjectInfo.GetNoPreAuths();
+            ObjectInfo.GetConstrainDelegations();
+            ObjectInfo.GetRPConstrainDelegations();
+            ObjectInfo.GetDomainTrusts();
+            // ObjectInfo.GetObjectACL(ldapPath);
+            ObjectInfo.GetNoAuthPasswordAccounts();
+            ObjectInfo.GetDomainAdminGroups();
+            ObjectInfo.IsInDomain();
+            ObjectInfo.GetGroupUser();
+
+
+        }
+
+
+    }
+}
